ce7838f709bcd86c08fd23847d41c32afaae659d
Mike Scott <mike@plokta.com>
2011-02-27 18:03:39 +0000

    Netatalk fixes

--- modules/netatalk/manifests/init.pp
+++ modules/netatalk/manifests/init.pp
@@ -4,10 +4,18 @@ class netatalk {
     }
     
     file { "/etc/ufw/applications.d/netatalk":
-        source => "puppet:///modules/netatalk/netatalk.ufw",
+        source  => "puppet:///modules/netatalk/netatalk.ufw",
 		require => Package["ufw"],
-        owner  => "root",
-        group  => "root",
+        owner   => "root",
+        group   => "root",
+    }
+
+    file { "/etc/netatalk/AppleVolumes.default":
+        source  => "puppet:///modules/netatalk/AppleVolumes.default",
+		require => Package["netatalk"],
+        owner   => "root",
+        group   => "root",
+        notify  => Service["netatalk"],
     }
 
     exec { "allow-netatalk":
@@ -16,4 +24,9 @@ class netatalk {
         require => [Exec["enable-firewall"], File["/etc/ufw/applications.d/netatalk"]],
     }
     
+    service { "netatalk":
+        ensure  => running, 
+        require => Package["netatalk"],
+        pattern => "afpd",
+    }
 }
